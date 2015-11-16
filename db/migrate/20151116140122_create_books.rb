cdc395fdff0b541ccbe30bb511f1540558c9b871
Brendan Lewis <lewis2ba@dukes.jmu.edu>
2015-11-16 09:07:44 -0500

    devise user and books migration created

---
+++ db/migrate/20151116140122_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.sting :author
+      t.string :genre
+      t.string :image_url
+      t.integer :no_pages
+      t.string :condition
+      t.string :synopsis
+    end
+  end
+end
